3d0f0ab31edeb42a608b15838ed9523b9b803af4
Ruina Z <rzhao9002@gmail.com>
2022-05-27 18:00:02 -0400

    added index.html

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
-import express from 'express'
+const express = require('express')
 const app = express()
 app.listen(3000, function() {
     console.log("listening on 3000")
+})
+
+app.get('/', (req, res) => { //request, response
+    console.log(__dirname)
+    res.sendFile(__dirname + "/index.html")
 })
