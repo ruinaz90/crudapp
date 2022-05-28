a0cc464afe88336e1d7159885640640c51970629
Ruina Z <rzhao9002@gmail.com>
2022-05-28 09:06:42 -0400

    CRUD-CREATE

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
-app.listen(3000, function() {
-    console.log("listening on 3000")
-})
+
+app.use(bodyParser.urlencoded({ extended: true}))
 
 app.get('/', (req, res) => { //request, response
     console.log(__dirname)
     res.sendFile(__dirname + "/index.html")
+})
+
+app.post('/quotes', (req, res) => {
+    console.log(req.body)
+})
+
+app.listen(3000, function() {
+    console.log("listening on 3000")
 })
