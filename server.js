517d8899714c2001d25251bf8b9a9da77dd8e41b
Ruina Z <rzhao9002@gmail.com>
2022-05-28 14:29:17 -0400

    MongoDB

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express')
+const MongoClient = require('mongodb').MongoClient
 const bodyParser = require('body-parser')
 const app = express()
+const connectionString = 'mongodb+srv://admin:pass@cluster0.1brh5bj.mongodb.net/?retryWrites=true&w=majority'
 
 app.use(bodyParser.urlencoded({ extended: true}))
+
+MongoClient.connect(connectionString, {
+    useUnifiedTopology: true })
+    .then(client => {
+        console.log("Connected to database")
+        const db = client.db('star-wars-quotes')
+        app.use()
+        app.get()
+        app.post()
+        app.listen()
+    })
+    .catch(error => console.error(error))
 
 app.get('/', (req, res) => { //request, response
     console.log(__dirname)
